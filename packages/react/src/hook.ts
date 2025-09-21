42ade0be61ee40e517c779f38295fc8b56b33f48
Erik Codev <hello@notco.dev>
2025-09-21 18:00:35 +0300

    feat: added onStart method to hook options

--- packages/react/src/hook.ts
+++ packages/react/src/hook.ts
@@ -1,27 +1,13 @@
-import type { TelegramUserData } from '@telegram-login-ultimate/core'
+import type {
+  LoginOptions,
+  TelegramUserData,
+} from '@telegram-login-ultimate/core'
 
 import { useContext, useState } from 'react'
 
 import { TelegramLoginContext } from './context'
 
-export interface UseTelegramLoginOptions {
-  /**
-   * The unique identifier of the Telegram bot. You can obtain it from the https://t.me/username_to_id_bot.
-   */
-  botId: number
-
-  /**
-   * Optional callback function that will be called if an error occurs during the login process.
-   * @param {unknown} error - The error that occurred.
-   */
-  onError?: (error: unknown) => void
-
-  /**
-   * Optional callback function that will be called when the login process completes successfully.
-   * @param {TelegramUserData} data - The authentication data returned from Telegram.
-   */
-  onSuccess?: (data: TelegramUserData) => void
-}
+export type UseTelegramLoginOptions = LoginOptions
 
 export interface UseTelegramLoginReturn {
   /**
@@ -39,6 +25,7 @@ export const useTelegramLogin = ({
   botId,
   onError,
   onSuccess,
+  onStart,
 }: UseTelegramLoginOptions): UseTelegramLoginReturn => {
   const [isPending, setIsPending] = useState(false)
   const client = useContext(TelegramLoginContext)
@@ -51,6 +38,7 @@ export const useTelegramLogin = ({
 
   const handleStart = (): void => {
     setIsPending(true)
+    onStart?.()
   }
 
   const handleError = (error: unknown): void => {
