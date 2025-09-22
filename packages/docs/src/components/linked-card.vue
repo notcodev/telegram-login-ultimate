7192174840c848ef34a0dfe83dc0c436c7f3e08f
Erik Codev <hello@notco.dev>
2025-09-22 23:53:21 +0300

    docs: changed framework card styles

--- packages/docs/src/components/linked-card.vue
+++ packages/docs/src/components/linked-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="rounded-xl border border-(--vp-c-divider) hover:border-(--vp-c-brand-1) flex w-full flex-col items-center p-6 transition-colors sm:p-10 text-(--vp-c-text-1)"
+    class="rounded-xl bg-neutral-50 hover:bg-neutral-50/80 dark:bg-neutral-900 dark:hover:bg-neutral-900/80 flex w-full flex-col items-center p-6 transition-colors sm:p-10 text-(--vp-c-text-1)"
   >
     <slot />
   </div>
